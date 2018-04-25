822b6f7c662c6309e0a033d96d9ac4e6c93b1af6
Mike <Judge73@yandex.ru>
2018-04-25 09:24:00 +0300

    Cleaning old commits

--- AirportService/src/main/java/com/someairlines/entity/util/EmployeeSort.java
+++
@@ -1,5 +0,0 @@
-package com.someairlines.entity.util;
-
-public enum EmployeeSort {
-	Id, FirstName, LastName, Job, Email, Free;
-}
--- AirportService/src/main/java/com/someairlines/entity/util/FlightSort.java
+++
@@ -1,5 +0,0 @@
-package com.someairlines.entity.util;
-
-public enum FlightSort {
-	Id, Origin, Destination, Date, Status
-}
